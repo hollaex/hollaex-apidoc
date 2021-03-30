75e1aecb7d7d4c7d3708d7f2d39a00b6db15c923
brandonkimmmm <bkim2490@gmail.com>
2021-03-30 18:42:06 +0900

    build.sh fix

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 docker run --rm --name slate \
 	-v $(pwd)/build:/srv/slate/build \
 	-v $(pwd)/source/index.html.md:/srv/slate/source/index.html.md \
-	-v $(pwd)/source/includes:/srv/slate/source/includes 
+	-v $(pwd)/source/includes:/srv/slate/source/includes \
 	slatedocs/slate build 
