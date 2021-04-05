db346433a581d083d1edfcb0ce517602c11c27ed
brandonkimmmm <bkim2490@gmail.com>
2021-04-05 18:02:34 +0900

    log image fix

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 docker run --rm --name slate \
 	-v $(pwd)/source/index.html.md:/srv/slate/source/index.html.md \
 	-v $(pwd)/source/includes:/srv/slate/source/includes \
+	-v $(pwd)/source/images:/srv/slate/source/images \
 	-p 4567:4567 \
 	slatedocs/slate serve 
